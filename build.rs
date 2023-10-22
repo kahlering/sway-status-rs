e401b25d67b1ac4e764bd8fb647691ee31ee69a9
kahlering <kevin.ahlering@outlook.com>
2023-10-22 20:57:31 +0200

    clean up build.rs

--- build.rs
+++ build.rs
@@ -1,18 +1,10 @@
-use std::env;
+//use std::env;
 
 const ALSA_SRC: &str = "src/status_modules/audio_module/alsa.c";
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed={}", ALSA_SRC);
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("{out_dir}");
-    
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let alsa_dir = dir + "/src/status_modules/audio_module";
-    println!("{}", &alsa_dir);
-
 
     cc::Build::new()
         .file(ALSA_SRC)
